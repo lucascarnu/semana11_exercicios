f90cb1586b12777d8e6a80f6370fa83374cacdb0
lucascarnu <lucascarnu@gmail.com>
2024-04-13 16:33:10 -0300

    git inicial project setup

---
+++ src/test/java/com/s11/exercicio_semanal/ExercicioSemanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s11.exercicio_semanal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSemanalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
